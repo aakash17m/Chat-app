29737432929f91d3a7ce2f885482183e9f45d46f
Aakash Maurya <aakashmourya65@gmail.com>
2025-05-29 15:40:38 +0530

    initialise

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,31 @@
-export const signup = (req, res) => {
-    res.send("signup route");
+import User from "../models/user.model.js"
+import bcrypt from "bcrypt";
+
+export const signup = async (req, res) => {
+    // res.send("signup route");
+    const { email, fullname, password } = req.body;
+    try {
+        if (password.length < 7) {
+            return res.status(400).json({ message: `Password must be at least 7 characters` });
+        }
+        const user = await User.findOne({email})
+
+        if(user){
+            return res.status(400).json({message: `Email already exixts`});
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullname,
+            email,
+            password: hashedPass
+        })
+        
+    } catch (error) {
+        
+    }
 }
 export const login = (req, res) => {
     res.send("login route");
