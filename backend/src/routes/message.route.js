6916cd5f5f690561ab0d303651c13915c02d4c3d
Aakash Maurya <aakashmourya65@gmail.com>
2026-09-13 22:44:29 +0530

    init

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar } from "../controllers/message.comtroller.js";
+import { sendMessage, getMessages, getUsersForSidebar } from "../controllers/message.comtroller.js";
 
 const router = express.Router();
 
 router.get("/User", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
