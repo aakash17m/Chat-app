b23e44782ca33cb0c7580ca3be194ce303d67d8f
Aakash Maurya <aakashmourya65@gmail.com>
2026-09-21 01:07:31 +0530

    init

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,7 +1,7 @@
-/* export default {
+export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-} */
-"@tailwindcss/vite"
+} 
+  /* "@tailwindcss/vite" */
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 import daisyui from 'daisyui';
 
 
-/* @type {import('tailwindcss').Config} */
+/**  @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
@@ -11,4 +11,5 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
+  
 }
