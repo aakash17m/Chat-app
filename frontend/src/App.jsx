5cae3d40519a0e873e49b42559ec5ee25e219402
Aakash Maurya <aakashmourya65@gmail.com>
2026-09-21 00:42:48 +0530

    init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+// import React from 'react'
 
 const App = () => {
   return (
-    <div className="text-red-500">Hello world</div>
+    <div className="text-red-500">Hello world
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </div>
   )
 }
 
