3b9ff82c6a71181d2f2fa7d6a777da1ba85c4600
Aakash Maurya <aakashmourya65@gmail.com>
2026-09-21 21:49:03 +0530

    init

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,7 +1,13 @@
-export default {
+/* export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-} 
+} */
   /* "@tailwindcss/vite" */
+
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
