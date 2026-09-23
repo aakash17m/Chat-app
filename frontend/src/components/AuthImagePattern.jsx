5c7025f9039cc2a84a6dfbbd5c15831a7ffb731c
Aakash Maurya <aakashmourya65@gmail.com>
2026-09-23 21:41:10 +0530

    init

---
+++ frontend/src/components/AuthImagePattern.jsx
@@ -0,0 +1,16 @@
+const AuthImagePattern = ({ title, subtitle }) => {
+    return (
+        <div className="hidden lg:flex justify-center items-center bg-base-200 p-12">
+            <div className="max-w-md text-center">
+                <div classname="grid grid-cols-3 gap-3 mb-8">
+                    {[...Array(9)].map((_, i) => (
+                        <div key={i} className={`aspect-square `}></div>
+                    ))}
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default AuthImagePattern;
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
  import { useState } from 'react';
  import { useAuthStore } from '../store/UseAuthStore';
- import { Mail, MessageSquare } from 'lucide-react';
+ import { EyeOff, Mail, MessageSquare } from 'lucide-react';
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -58,9 +58,54 @@ const SignUpPage = () => {
               </div>
             </div>
 
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Password</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Lock className="size-5 text-base-content/40" />
+                </div>
+                <input type={showPassword ? "text" : "password"} className={`input input-bordered w-full pl-10`} placeholder="••••••••" value={formData.password} onChange={(e) => setFormData({ ...formData, password: e.target.value})} />
+                <button type="button" onClick={() => setShowPassword(!showPassword)} className="absolute inset-y-0 right-0 pr-3 flex items-center">
+                  {showPassword ? (
+                    <EyeOff className="size-5 text-base-content/40" />) : (
+                    <Eye className="size-5 text-base-content/40" />
+                    )}
+                </button>
+              </div>
+            </div>
+            <button type="submit" className="btn btn-primary w-full" disabled={isSigningUp}>
+              {isSigningUp ? (
+                <>
+                  <Loader2 className="size-5 animate-spin" />
+                  Loading...
+                </>
+              ) : (
+                "Create Account"
+              )}
+            </button>
           </form>
+
+          <div className="text-center">
+            <p className="text-sm text-base-content/60">
+              Already have an account?{' '}
+              <Link to="/login" className="link link-primary">
+                Sign in
+              </Link>
+            </p>
+          </div>
+
         </div>
       </div>
+
+      {/* Right Side */}
+
+      <AuthImagePattern
+        title="Welcome to ChatApp"
+        subtitle="Connect with friends and stay in touch with your loved ones."
+      />
+
     </div>
   )
 }
